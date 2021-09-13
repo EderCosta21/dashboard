55c5ad032a4c10dbbdadb28de40b07d5401facca
eder6 <eder@b2mlportal.com.br>
2021-09-13 14:18:55 -0300

    Atualização

--- src/app/dashboard/dados.service.ts
+++ src/app/dashboard/dados.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class DadosService {
 
-  constructor() { }
+    readonly dados = [
+        ['Janeiro', 33],
+        ['Fevereiro', 68],
+        ['Março', 49],
+        ['Abril', 15],
+        ['Maio', 80],
+        ['Junho', 27],
+        ['Julho', 79],
+        ['Agosto', 11],
+        ['Setembro', 60],
+        ['Outubro', 89],
+        ['Novembro', 41],
+        ['Dezembro', 10],
+    ];
+
+	constructor() {}
+
+	/**
+	 * Retorna um observable contendo os dados a serem
+	 * exibidos no gráfico.
+	 *
+	 * @return Observable<any>
+	 */
+	obterDados(): Observable<any> {
+		return new Observable(observable => {
+			observable.next(this.dados);
+			observable.complete();
+		});
+	}
+
 }
